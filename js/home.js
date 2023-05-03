ff7d5a1ee408e48db7e597a806294306a9b7c47a
apple <apple@apples-MacBook-Pro.local>
2023-05-03 13:58:28 +0900

    remove session storgae

--- js/home.js
+++ js/home.js
@@ -1,11 +1,8 @@
 const greeting = document.querySelector('.greeting');
 
 window.onload = () => {
-    if(!sessionStorage.name){
+
         location.href = '/login';
-    } else{
-        greeting.innerHTML = `hello ${sessionStorage.name}`;
-    }
 }
 
 const logOut = document.querySelector('.logout');
