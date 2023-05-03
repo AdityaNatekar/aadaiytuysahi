f4faac21e43b352dc23afe34ef16835057a44284
apple <apple@apples-MacBook-Pro.local>
2023-05-03 18:24:57 +0900

    home page issue solved4

--- js/form.js
+++ js/form.js
@@ -8,28 +8,6 @@ form.forEach((item, i) => {
     }, i*100);
 })
 
-window.onload = () => {
-    if(sessionStorage.name){
-        location.href = '/';
-    }
-}
-
-const hardcodedUsername = "AAA";
-const hardcodedPassword = "AAA";
-
-function login() {
-    const username = document.getElementById("username").value;
-    const password = document.getElementById("password").value;
-
-    if (username === hardcodedUsername && password === hardcodedPassword) {
-        alert("Login successful!");
-        // Redirect the user to the desired page
-        window.location.href = "/login.html";
-    } else {
-        alert("Invalid username or password");
-    }
-}
-
 
 const alertBox = (data) => {
     const alertContainer = document.querySelector('.alert-box');
